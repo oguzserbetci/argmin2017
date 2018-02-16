88f94bd03e729007f9f6369a69d42ccf959e061b
Oguz Serbetci <oguz.serbetci@gmail.com>
2018-02-16 13:48:45 +0100

    clean

--- utils.py
+++ utils.py
@@ -9,11 +9,10 @@ class Metrics(Callback):
         self.metrics = defaultdict(list)
 
     def on_epoch_end(self, batch, logs={}):
-        seq_ind = np.where(~self.validation_data[0].any(axis=1))[0]
-        print(seq_ind)
         predict = self.model.predict(self.validation_data[0])
-        link_predict = predict.argmax(2).flatten()
-        link_target = self.validation_data[1].argmax(2).flatten()
+        mask_ind = self.validation_data[2].any(axis=2).flatten()
+        link_predict = predict.argmax(2).flatten()[mask_ind]
+        link_target = self.validation_data[1].argmax(2).flatten()[mask_ind]
         self.metrics['link_macro_f1'].append(f1_score(link_target, link_predict, average='macro'))
         for k, v in logs.items():
             self.metrics[k].append(v)
@@ -24,14 +23,12 @@ class JointMetrics(Callback):
         self.metrics = defaultdict(list)
 
     def on_epoch_end(self, batch, logs={}):
-        print(self.validation_data[1].shape)
-        seq_ind = np.where(~self.validation_data[1].any(axis=2))[0]
-        print(seq_ind)
         predict = self.model.predict(self.validation_data[0])
-        link_predict = predict[0].argmax(2).flatten()
-        type_predict = predict[0].argmax(2).flatten()
-        link_target = self.validation_data[1].argmax(2).flatten()
-        type_target = self.validation_data[2].argmax(2).flatten()
+        mask_ind = self.validation_data[2].any(axis=2).flatten()
+        link_predict = predict[0].argmax(2).flatten()[mask_ind]
+        type_predict = predict[0].argmax(2).flatten()[mask_ind]
+        link_target = self.validation_data[1].argmax(2).flatten()[mask_ind]
+        type_target = self.validation_data[2].argmax(2).flatten()[mask_ind]
         self.metrics['link_macro_f1'].append(f1_score(link_target, link_predict, average='macro'))
         self.metrics['type_macro_f1'].append(f1_score(type_target, type_predict, average='macro'))
         # self.metrics['type_ind_f1'].append(f1_score(type_target, type_predict, labels=[0,1], average=None))
