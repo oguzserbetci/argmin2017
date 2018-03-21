b3abe8bdb8f2b8e6f811e7e5b37dc579a6d5a9dd
Oguz Serbetci <oguz.serbetci@gmail.com>
2018-03-21 17:11:35 +0100

    implement encoder according to translation tutorial.

--- pytorch.py
+++ pytorch.py
@@ -3,3 +3,23 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embeddin(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)  # .view returns the same data in another shape -1 infers size
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        return result
+
