e59362950934509e69228ada873dad26ac650a20
Oguz Serbetci <oguz.serbetci@gmail.com>
2018-03-21 22:25:37 +0100

    complete pytorch tutorial

--- pytorch.py
+++ pytorch.py
@@ -4,17 +4,20 @@ from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
 
+MAX_LEN = 7
+
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embeddin(input_size, hidden_size)
+        self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
 
     def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)  # .view returns the same data in another shape -1 infers size
+        # .view returns the same data in another shape -1 infers size
+        embedded = self.embedding(input).view(1, 1, -1)
         output = embedded
         output, hidden = self.gru(output, hidden)
         return output, hidden
@@ -23,3 +26,65 @@ class EncoderRNN(nn.Module):
         result = Variable(torch.zeros(1, 1, self.hidden_size))
         return result
 
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        # input is its target from last time step, i.e. output_size = input_size
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        # output has all h_t up until t
+        # here we take the last timestep
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def init_hidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        return result
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LEN):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size, * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)  # apply dropout always before the layer)
+
+        attn_weights = F.softmax(self.attn(torch.cat((embedded[0], hidden[0]), 1)),
+                                 dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def init_hidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        return result
