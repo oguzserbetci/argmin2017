2768043ccad2c7a74a4bfc430a4d312ed356a9ac
Oguz Serbetci <oguz.serbetci@gmail.com>
2018-03-21 16:52:19 +0100

    init file

---
+++ pytorch.py
@@ -0,0 +1,11 @@
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
